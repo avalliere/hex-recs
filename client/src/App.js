22bcc296261095de55ccd66013d7c219c263b6cf
Andrea Valliere <amvalliere@gmail.com>
2021-07-25 23:08:54 -0700

    adds colors with color-specific recommendations attributes

--- client/src/App.js
+++ client/src/App.js
@@ -3,40 +3,74 @@ import './App.css';
 import Color from './components/Color';
 
 function App() {
-
-  const [recs, setRecs] = useState([])
-  const [selectedColor, setSelectedColor] = useState()
+  const [recs, setRecs] = useState([]);
+  const [selectedColor, setSelectedColor] = useState();
 
   const paramLookup = {
-    '#f5f0af': { min_tempo: 140 },
-    '#0f50af': { min_tempo: 80 }
-  }
+    '#f0826c': { min_tempo: 140 },
+    '#98f06c': { min_tempo: 80 },
+    '#6cdaf0': {},
+    '#160c1b': {},
+    '#8f6cf0': {},
+  };
 
   const getSpotifyRecs = async () => {
-    const params = paramLookup[selectedColor];
+    // const params = paramLookup[selectedColor];
+    const params = { selectedColor };
+    // { selectedColor: '#...' }
+    await fetch(
+      '/recommendations?' +
+        // new URLSearchParams({
+        //   ...params,
 
-    await fetch('/recommendations?' + new URLSearchParams({
-      ...params
-    }))
-    .then(res => res.json())
-    .then(data => {
-      console.log(data.recs.tracks)
-      setRecs(data.recs.tracks)
-    })
-    .catch(err => {throw new Error(err)})
-  }
-
+        // })
+        new URLSearchParams(params)
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.recs.tracks);
+        setRecs(data.recs.tracks);
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  };
 
   return (
     <div className="App">
       <button onClick={() => getSpotifyRecs()}>Get recs</button>
-      <Color selectedColor={selectedColor} setSelectedColor={setSelectedColor} colorHex='#f5f0af' />
-      <Color selectedColor={selectedColor} setSelectedColor={setSelectedColor} colorHex='#0f50af' />
-     {recs?.map(rec=> {
-      return(
-        <p key={rec.id}>{rec.artists[0].name} : {rec.name}</p>
-      )
-    })}   
+      <Color
+        selectedColor={selectedColor}
+        setSelectedColor={setSelectedColor}
+        colorHex="#f0826c"
+      />
+      <Color
+        selectedColor={selectedColor}
+        setSelectedColor={setSelectedColor}
+        colorHex="#98f06c"
+      />
+      <Color
+        selectedColor={selectedColor}
+        setSelectedColor={setSelectedColor}
+        colorHex="#6cdaf0"
+      />
+      <Color
+        selectedColor={selectedColor}
+        setSelectedColor={setSelectedColor}
+        colorHex="#160c1b"
+      />
+      <Color
+        selectedColor={selectedColor}
+        setSelectedColor={setSelectedColor}
+        colorHex="#8f6cf0"
+      />
+      {recs?.map((rec) => {
+        return (
+          <p key={rec.id}>
+            {rec.artists[0].name} : {rec.name}
+          </p>
+        );
+      })}
     </div>
   );
 }
--- client/src/components/Color.js
+++ client/src/components/Color.js
@@ -1,12 +1,15 @@
-const Color = ({selectedColor, setSelectedColor, colorHex}) => {
-  const isSelected = selectedColor === colorHex; 
+const Color = ({ selectedColor, setSelectedColor, colorHex }) => {
+  const isSelected = selectedColor === colorHex;
 
   return (
-    <div 
-      className='color-square'  
-      style={{backgroundColor: colorHex, border: isSelected ? '1px solid red' : ''}} 
-      onClick={() => setSelectedColor(colorHex)}>
-    </div>
-  )
-}
+    <div
+      className="color-square"
+      style={{
+        backgroundColor: colorHex,
+        border: isSelected ? '1px solid red' : '',
+      }}
+      onClick={() => setSelectedColor(colorHex)}
+    ></div>
+  );
+};
 export default Color;
