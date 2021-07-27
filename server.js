b7d97e35d21f225233d22e718e12e7973b9d86f7
Andrea Valliere <amvalliere@gmail.com>
2021-07-26 19:27:01 -0700

    updates build script to use npm; adds root get to serve react app

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
