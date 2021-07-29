d6eb34c0e3717dbde1f17c8b55dfab24cf13e376
Andrea Valliere <amvalliere@gmail.com>
2021-07-28 18:28:52 -0700

    fixes spacing of track list; gives tracks consistent black background

--- client/src/components/Track.js
+++ client/src/components/Track.js
@@ -1,12 +1,13 @@
 const Track = (rec) => {
   const recId = rec.rec.id;
-  const url = `https://open.spotify.com/embed/track/${recId}`;
+  const url = `https://open.spotify.com/embed/track/${recId}?theme=0`;
 
   return (
-    <div style={{ margin: '20px 0' }}>
+    <div style={{ margin: '5px 0' }}>
       <iframe
         title={recId}
         src={url}
+        theme="0"
         width="100%"
         height="75"
         frameBorder="0"
